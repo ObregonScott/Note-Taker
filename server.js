68bf4afc8369f67c18805b4cb53640747208be62
Scott Obregon <obregon.scott@gmail.com>
2022-07-22 22:58:23 -0500

    added dependencies to server

--- server.js
+++ server.js
@@ -1,10 +1,3 @@
 //Dependencies
-const fs = require('fs');
-const express = require('express');
-
-const path = require('path');
-const util = require('util')
-
-//Server SetUp
+const express = require('express')
 const app = express();
-const PORT = process.env.PORT || 3001;
