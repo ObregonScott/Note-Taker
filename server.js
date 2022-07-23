7ecf233eb9bef2c28b94be0b9d353f83016964d7
Scott Obregon <obregon.scott@gmail.com>
2022-07-22 23:03:39 -0500

    api and html routes added

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 //Dependencies
 const express = require('express')
 const app = express();
+
+// HTML routes
+const htmlRoutes = require("./routes/htmlRoutes");
+// API routes
+const apiRoutes = require("./routes/apiRoutes");
