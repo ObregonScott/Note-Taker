c4ffdab859a74e7fbb734cf94eedcd4ad596c323
Scott Obregon <obregon.scott@gmail.com>
2022-07-23 10:15:01 -0500

    made new branch for heroku retry

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,14 @@
 //Dependencies/Tools/and Required files
 const router = require('express').Router();
 const path = require('path');
-const notes = require('./Develop/db/db.json');
+const notes = require('./db/db.json');
 const {v4: uuidv4} = require('uuid');
 //Asynchronous Processes
 const fs = require('fs');
 
 // API Route | Get Request
 router.get('/notes', (req, res) => {
-  fs.readFile(path.join(__dirname, './Develop/db/db.json'), (err,data) => {
+  fs.readFile(path.join(__dirname, './db/db.json'), (err,data) => {
   if(err) res.sendStatus(404);
   console.log(data);
   res.json(JSON.parse(data));
@@ -21,11 +21,11 @@ router.get('/notes', (req, res) => {
 router.post('/notes', (req, res) => {
   const newNotes = req.body
   newNotes.id = uuidv4();
-  let readData = (path.join(__dirname,('./Develop/db/db.json', "utf8")));
+  let readData = (path.join(__dirname,('./db/db.json', "utf8")));
 
   notes.push(newNotes);
 
-  fs.writeFile('./Develop/db/db.json', JSON.stringify(notes), err => {
+  fs.writeFile('./db/db.json', JSON.stringify(notes), err => {
     if (err) { res.sendStatus(404);
     } else {
       console.log('Note saved.');
@@ -38,7 +38,7 @@ res.json(readData);
 // API Route | Delete Request
 router.delete('/notes/:id', (req, res) => {
   let noteId = req.params.id;
-  let readData = (path.join(__dirname('./Develop/db/db.json', "utf8")));
+  let readData = (path.join(__dirname('./db/db.json', "utf8")));
 
   let findData = readData.filter(note => note.id.length !== note)
 });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,13 +3,13 @@ const path = require('path');
 
 // GET notes HTML page
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
- });
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+//  });
 router.get('*', (req, res) => {
-    res.sendFile(__dirname, ".s/Develop/public/index.html");
+    res.sendFile(__dirname, "../public/index.html");
 })
 
 module.exports = router;
